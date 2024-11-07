a169c0a1e875a2c6847b7fcb2ec4b83451cf22fc
danichagas <chagasdaniel788@gmail.com>
2024-11-07 13:22:26 -0300

    feat: data reading

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Digite os dados do produto: ");
+        System.out.print("Nome: ");
+        product.name = sc.nextLine();
+        System.out.print("Preço: ");
+        product.price = sc.nextDouble();
+        System.out.println("Quantidade: ");
+        product.quantity = sc.nextInt();
+
+
+
+
+        sc.close();
 
     }
 }
