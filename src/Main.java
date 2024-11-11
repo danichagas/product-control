56e209440c6bbf6d9657cef08c0976572f3578c2
danichagas <chagasdaniel788@gmail.com>
2024-11-11 13:51:31 -0300

    feat: using constructors

--- src/Main.java
+++ src/Main.java
@@ -9,22 +9,22 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
         System.out.println("Digite os dados do produto: ");
         System.out.print("Nome: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.print("Preço: ");
-        product.price = sc.nextDouble();
+        double price = sc.nextDouble();
         System.out.print("Quantidade: ");
-        product.quantity = sc.nextInt();
+        int quantity = sc.nextInt();
 
+        Product product = new Product(name, price, quantity);
 
         System.out.println("Dados do produto: " + product);
 
         System.out.println();
 
         System.out.println("Digite um número de produtos a serem adicionados no estoque: ");
-        int quantity = sc.nextInt();
+        quantity = sc.nextInt();
         product.addProducts(quantity);
 
         System.out.println("Dados do produto: " + product);
