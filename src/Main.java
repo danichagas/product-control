3be4a09eb6dff29d778211019e7f10aa683547ba
danichagas <chagasdaniel788@gmail.com>
2024-11-11 14:30:23 -0300

    feat: overload

--- src/Main.java
+++ src/Main.java
@@ -14,17 +14,15 @@ public class Main {
         String name = sc.nextLine();
         System.out.print("Preço: ");
         double price = sc.nextDouble();
-        System.out.print("Quantidade: ");
-        int quantity = sc.nextInt();
 
-        Product product = new Product(name, price, quantity);
+        Product product = new Product(name, price);
 
         System.out.println("Dados do produto: " + product);
 
         System.out.println();
 
         System.out.println("Digite um número de produtos a serem adicionados no estoque: ");
-        quantity = sc.nextInt();
+        int quantity = sc.nextInt();
         product.addProducts(quantity);
 
         System.out.println("Dados do produto: " + product);
